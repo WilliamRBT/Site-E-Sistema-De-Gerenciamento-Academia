938bc946af0208b1b624358c279e2c3c1ec79692
[dyad] <willrbt98@uni9.edu.br>
2025-09-24 16:06:27 -0300

    Reverted all changes back to version 3dde9252a6d9cdab9469742911ba5790f87ade6d

---
+++ components/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as DumbbellIcon } from './DumbbellIcon';
+export { default as UsersIcon } from './UsersIcon';
+export { default as HeartPulseIcon } from './HeartPulseIcon';
+export { default as QuoteIcon } from './QuoteIcon';
