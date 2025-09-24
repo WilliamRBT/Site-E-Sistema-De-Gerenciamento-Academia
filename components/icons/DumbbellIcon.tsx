addc2cb667fe81edc8c85a103c75d03d9ffb4f03
[dyad] <willrbt98@uni9.edu.br>
2025-09-24 16:06:54 -0300

    [dyad] Removendo arquivos duplicados para corrigir o roteamento dos planos. - deleted 15 file(s)

--- components/icons/DumbbellIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DumbbellIcon: React.FC = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.253v11.494m-6.253-5.747H18.253M5.747 6.253h.01M18.253 6.253h.01M5.747 17.747h.01M18.253 17.747h.01" />
-  </svg>
-);
-
-export default DumbbellIcon;
