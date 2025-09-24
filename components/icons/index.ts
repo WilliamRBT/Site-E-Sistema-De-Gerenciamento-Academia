9b9b3ab7ce23cd5698839616e40cce920b292f9c
[dyad] <willrbt98@uni9.edu.br>
2025-09-24 16:05:51 -0300

    [dyad] Removida pasta de componentes duplicada para corrigir a navegação dos planos. - deleted 12 file(s)

--- components/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as DumbbellIcon } from './DumbbellIcon';
-export { default as UsersIcon } from './UsersIcon';
-export { default as HeartPulseIcon } from './HeartPulseIcon';
-export { default as QuoteIcon } from './QuoteIcon';
